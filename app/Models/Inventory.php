8623865afd4484e108fa654270427e4db3ffd10b
Amawers <dacoroon.christianjericho@gmail.com>
2025-03-27 16:56:06 +0800

    feat(inventory): added mode for storing data with custom function for asset_tag generation

---
+++ app/Models/Inventory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $table = 'inventory';
+
+    protected $fillable = [
+        'user_id',
+        'serial_number',
+        'asset_name',
+        'asset_type',
+        'status',
+        'date_acquired',
+        'deployed_date',
+        'user_incharge',
+        'remarks'
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($inventory) {
+            $year = substr($inventory->date_acquired, 2, 2);
+            $assetType = strtoupper(substr($inventory->asset_type, 0, 3));
+            $count = Inventory::where('asset_type', $inventory->asset_type)->count() + 1;
+
+            $inventory->asset_tag = "MCT{$year}-{$assetType}" . str_pad($count, 3, '0', STR_PAD_LEFT);
+        });
+    }
+
+}
