b4bcf6a67f572ee0f857fdaa1a58e7b78d451a47
Amawers <dacoroon.christianjericho@gmail.com>
2025-04-08 16:48:59 +0800

    feat(inventory): date converter to string

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,10 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
-
+import { clsx } from 'clsx';
+import moment from 'moment';
+import { twMerge } from 'tailwind-merge';
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
+}
+export function getDateString(date) {
+    //! also singapore time
+    return moment(date).format('MMMM Do YYYY');
 }
